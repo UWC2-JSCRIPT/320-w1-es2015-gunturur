c578735d5404fab0d422c052990cba13bc26d3b6
Gunturu <guntur@88665a4d2f5a.ant.amazon.com>
2023-01-23 22:40:02 -0800

    assignemnt 1

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,15 @@
 // Import HTMLElement here
-
+import HTMLElement from './HTMLElement';
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(props) {
+    super(props);
+    this.content = super.content;
+  }
 
+  render() {
+    return <div>`{super.render(this.content)}`</div>;
+  }
+}
 // Export class here
+export default DivElement;
